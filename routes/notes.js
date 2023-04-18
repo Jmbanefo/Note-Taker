7ef1e49caa28ae1487ae63ae75e30db0e92054d9
Justin <mbanefojustin@gmail.com>
2023-04-18 17:19:25 -0500

    correct routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,45 +1,47 @@
-const router = require('express').Router(); 
-const util = require('util'); 
-const fs = require('fs'); 
-const { v4: uuidv4 } = require('uuid'); 
+const router = require('express').Router();
+const util = require('util');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const { format } = require('path');
 
 
-const writeToFile = util.promisify(fs.writeFile); 
-const readFromFile = util.promisify(fs.readFile); 
+const writeToFile = util.promisify(fs.writeFile);
+const readFromFile = util.promisify(fs.readFile);
 
 
-router.get('/', (req, res) => {
+router.get('/notes', (req, res) => {
     readFromFile('./db/db.json')
-    .then((data) =>
-    res.json(JSON.parse(data))); 
-}) 
+        .then((data) =>
+            res.json(JSON.parse(data)));
+})
 
-router.post('/', (req, res) => { 
-    const {title, text} = req.body; 
-    const newNote = { 
-        title, 
-        text, 
-        id: uuidv4(), 
+router.post('/notes', (req, res) => {
+    const { title, text } = req.body;
+    const newNote = {
+        title,
+        text,
+        id: uuidv4(),
     };
-    
+
     readFromFile('./db/db.json', 'utf-8').then((data) => {
         console.log(newNote)
-        console.log(typeof(data))
-        const parsedData = JSON.parse(data); 
+        console.log(typeof (data))
+        const parsedData = JSON.parse(data);
         console.log(parsedData)
-    parsedData.push(newNote)
-    console.log("New Data", parsedData)
-    writeToFile('./db/db.json', JSON.stringify(parsedData))
-    res.json(JSON.parse(parsedData))
+        parsedData.push(newNote)
+        console.log("New Data", parsedData)
+        writeToFile('./db/db.json', JSON.stringify(parsedData))
+        res.json((parsedData))
     })
 })
 
 
 // router.delete('/notes/:id', (req, res) => { 
 //     const deleteID = req.params.id; 
+//     const noted = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); 
+//     fs.
 
 // })
 
-module.exports = router; 
+module.exports = router;
 
